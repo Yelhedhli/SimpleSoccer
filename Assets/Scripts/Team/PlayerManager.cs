ad68cb7b71f3c273b3ed96110bd0ad81662e36b2
Yassine Elhedhli <yassine.elhedhli@gmail.com>
2023-01-05 13:54:00 -0500

    basic object collider, and collider visualizer

--- Assets/Scripts/Team/PlayerManager.cs
+++ Assets/Scripts/Team/PlayerManager.cs
@@ -3,26 +3,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class PlayerManager : MonoBehaviour
 {
     public PlayerController[] teamPlayers;
     public PlayerController activePlayer;
+    
     private int index = 0;
+
+    private Vector3 capsuleStart;
+    private Vector3 capsuleEnd;
+    
+    [SerializeField]
+    private int capsuleRadius = 5;
+
     // Start is called before the first frame update
     void Start()
     {
         teamPlayers = GetComponentsInChildren<PlayerController>();
         activePlayer = teamPlayers[0];
         teamPlayers[0].SetPlayerControlled();
+
+        capsuleStart = activePlayer.transform.position;
+        capsuleEnd = new Vector3(0,0,0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        capsuleStart = activePlayer.transform.position;
         if(Input.GetButtonDown("SwitchPlayer")){
+            Collider[] targetPlayers = GetTargets();
+            foreach(Collider c in targetPlayers){
+                print(c.transform.parent);
+            }
             index = (index+1)%3;
-            print(index);
             SwitchPlayer(teamPlayers[index]);
+        }
+    }
+
+    void OnDrawGizmos(){
+        if (activePlayer != null){
+            GizmoHelper.DrawWireCapsule(capsuleStart, capsuleEnd, capsuleRadius);
         }
     }
 
@@ -30,6 +52,9 @@ public class PlayerManager : MonoBehaviour
         activePlayer.SetAIControlled();
         activePlayer = target;
         activePlayer.SetPlayerControlled();
-        
+    }
+
+    Collider[] GetTargets(){
+        return Physics.OverlapCapsule(capsuleStart, capsuleEnd, capsuleRadius);
     }
 }
