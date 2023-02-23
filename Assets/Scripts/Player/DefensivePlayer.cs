cffa913da4842e4f4878e86f3c574508ac8fb68a
Yassine Elhedhli <yassine.elhedhli@gmail.com>
2023-02-23 18:30:28 -0500

    creates defensive player class, moves offensive/defensive player classes into different prefabs

---
+++ Assets/Scripts/Player/DefensivePlayer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefensivePlayer : PlayerController
+{
+    public override void OffenseFixedUpdate(){
+        resetPosition(2.5f);
+    }
+
+    public override void DefenseFixedUpdate(){
+        resetPosition(2.5f);
+    }
+}
